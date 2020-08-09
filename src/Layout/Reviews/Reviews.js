0f88652450b7c61847d09d8962ae7383568758ae
clakika <31564756+clakika@users.noreply.github.com>
2020-08-09 16:42:29 +0200

    additional things added to the review section

--- src/Layout/Reviews/Reviews.js
+++ src/Layout/Reviews/Reviews.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './Reviews.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
+import testimonialOne from "../../img/testimonialOne.jpg";
 
 export class Reviews extends Component {
     render() {
@@ -11,10 +10,12 @@ export class Reviews extends Component {
                     <h2>Our testimonials</h2>
                 <div className="Reviews__Container">
                     <div className="Testimonial TestimonialOne">
-                        <FontAwesomeIcon icon={faQuoteLeft}/>
                         <p><q>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus in hendrerit gravida rutrum quisque.</q></p>
-                        <img></img>
-                        <p>Alessandra Wolf</p>
+                        <div className="UserReview">
+                            <img src={testimonialOne}></img>
+                            <p>Alessandra Wolf</p>
+                        </div>
+                        
                     </div>
 
                     <div className="Testimonial TestimonialTwo">
